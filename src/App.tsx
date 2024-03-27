6f65b878657859c4d746860c39bfb3b7815bef18
Tinsa <tinsa.baby@whitehatjr.com>
2024-03-27 21:11:41 +0530

    first simple typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { json } from 'stream/consumers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App:React.FC=()=>{
+
+  interface jsonlist{
+    "userId":number,
+    "title":string
+  }
+  const[list,setList]=useState<jsonlist[]>([])
+
+  useEffect(()=>{
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((res:Response)=>{
+      return res.json()
+    })
+    .then((res:any[])=>{
+      const list1:jsonlist[]=res.map((ind:any)=>{
+          return {"userId":ind["userId"],"title":ind["title"]}
+        }
+        )
+        setList(list1)
+    })
+    .catch((err:Error)=>{
+      console.error(err)
+    })
+  },[])
+ 
+  return(
+    <>
+    <div className='App'>Hello World</div>
+    <ul>
+      {list.length>0?(
+        list.map((lis:jsonlist)=>(
+          <li key={lis.userId}>{lis.title}</li>
+        ))
+      ):"No items found"}
+    </ul>
+    </>
   );
 }
+ 
+
 
 export default App;
